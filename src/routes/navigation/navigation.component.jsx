778d8b3b4525f02d8da6ee63233d10c87e3cc4bd
manuraj1107 <manurajrockstar2000@gmail.com>
2022-10-03 14:38:10 +0530

    Added the cart context and carts dropdown feature

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,17 @@
 import { Fragment, useContext } from 'react';
 import {Outlet, Link } from 'react-router-dom';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { CartContext } from '../../contexts/cart.context';
 import './navigation.styles.scss'
 
 
 const Navigation = () => {
   const {currentUser , setCurrentUser} = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
   const signOutHandler = async () => {
     await signOutUser();
     setCurrentUser(null)
@@ -35,7 +40,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
