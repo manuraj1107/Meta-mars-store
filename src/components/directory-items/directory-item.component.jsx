4557a3716a0de2c8f5d23f014ce820fc27bb64e2
manuraj1107 <manurajrockstar2000@gmail.com>
2022-10-08 22:26:39 +0530

    Refactor: updated the styling clashes, changed folder name from Category to Directory

---
+++ src/components/directory-items/directory-item.component.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './directory-item.styles.scss';
+
+const DirectoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className='directory-item-container'>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='body'>
+        <h2>{title}</h2>
+        
+        <Link to={`shop/${title}`}><p>Shop Now</p></Link>
+      </div>
+    </div>
+  );
+};
+
+export default DirectoryItem;
