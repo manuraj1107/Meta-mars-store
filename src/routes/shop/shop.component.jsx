8b7159cc1615031ff9077cb01ebc1f271a732c68
manuraj1107 <manurajrockstar2000@gmail.com>
2022-11-17 22:29:46 +0530

    Applied the concept of caching and memoization with redux-reselect library

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-import { setCategoriesMap } from '../../store/categories/categories.action';
+import { setCategories } from '../../store/categories/categories.action';
 
 import CategoriesPreview from '../categories-preview/categories-preview.components';
 import Category from '../category/category.component';
@@ -15,7 +15,7 @@ const Shop = () => {
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments('categories');
-      dispatch(setCategoriesMap(categoryMap));
+      dispatch(setCategories(categoryMap));
     };
 
     getCategoriesMap();
